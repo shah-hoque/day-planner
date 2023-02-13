2226b8443cf78bc9739a2e860a23ebdad62d7dbe
shah_hoque1 <shah.hoque10@gmail.com>
2023-02-13 17:11:15 +0000

    func successful at changing col at x time

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-// Get todays day and parse into id
+// Runs func's after DOM elements are loaded
 $(document).ready(function () {
+
+  // Get todays day and parse into id
   const todaysDay = moment().format("dddd");
   $("#day-today").text(todaysDay);
-});
 
 
 
+const BlocksAsTime = ["9:00am","10:00am","11:00am","12:00pm","1:00pm","2:00pm","3:00pm","4:00pm","5:00pm"]
 
+const allBlocksIds = ["#num9","#num10","#num11","#num12","#num1","#num2","#num3","#num4","#num5"]
 
+console.log(allBlocksIds)
 
 
 function timeChecker() {
   const currentDateTime = moment();
-  const futureDateTime = moment("4:01pm", "h:mmA");
 
-  if (currentDateTime.isBefore(futureDateTime)) {
-    console.log("time not reached yet");
-  } else if (currentDateTime.isSame(futureDateTime)) {
-    console.log("it's same");
-  } else {
-    console.log("already past that time");
+  for(let i = 0; i < BlocksAsTime.length; i++) {
+
+    let blockTime = moment(BlocksAsTime[i], "h:mm a")
+
+    if (currentDateTime.isAfter(blockTime)) {
+      console.log(allBlocksIds[i])
+
+
+      $(allBlocksIds[i]).css('background-color', 'blue')
+    }
+
   }
+
+
 }
+// setInterval(timeChecker, 10000);
 
-setInterval(timeChecker, 1000);
 
+
+timeChecker()
+
+
+
+
+});
