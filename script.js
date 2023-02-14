36a3e854cd22e7235779c5ef38c27827bbbd542b
shah_hoque1 <shah.hoque10@gmail.com>
2023-02-14 15:52:22 +0000

    User input/changes added to local storage

--- script.js
+++ script.js
@@ -10,25 +10,34 @@ $("input").on("input", function () {
   $("#saveChangeBtn").show();
 });
 
+// save user inputs / changes to local storage
+$("#saveChangeBtn").on("click", function () {
+  $("input").each(function () {
+    // save key as (this) id & value pair as (this) val
+    localStorage.setItem($(this).attr("id"), $(this).val());
+  });
+  $("#saveChangeBtn").hide();
+});
+
 
 // EVENT LISTENERS (end) -----------------------------------
 
 
 
-// Get todays day and parse into id
+// get todays day and parse into id
 const todaysDay = moment().format("dddd");
 $("#day-today").text(todaysDay);
 
 
-// Array of times to check against
+// array of times to check against
 const BlocksAsTime = ["9:00am","10:00am","11:00am","12:00pm","1:00pm","2:00pm","3:00pm","4:00pm","5:00pm"]
 
 
-// Array of all 9 input textarea to apply style to
+// array of all 9 input textarea to apply style to
 const allBlocksIds = ["#num9","#num10","#num11","#num12","#num1","#num2","#num3","#num4","#num5"]
 
 
-// Colour textarea if an hr has passed or it's the current hr
+// colour textarea if an hr has passed or it's the current hr
 function timeChecker() {
   const currentDateTime = moment(); // gets current day/time
 
@@ -45,11 +54,6 @@ function timeChecker() {
   }
 }
 setInterval(timeChecker, 1000); // runs the timeChecker func every second
-
-
-
-
-
 
 
 
