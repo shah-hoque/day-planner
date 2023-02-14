9304c68e4af5a4f7b08d05f518c02095385320a5
shah_hoque1 <shah.hoque10@gmail.com>
2023-02-14 15:42:59 +0000

    E l... added on #saveChangeBtn to show

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-// Runs func's after DOM elements are loaded
+// Runs code after DOM elements are loaded
 $(document).ready(function () {
 
-  // Get todays day and parse into id
-  const todaysDay = moment().format("dddd");
-  $("#day-today").text(todaysDay);
+$("#saveChangeBtn").hide();
+
+// EVENT LISTENERS -----------------------------------
+
+// checks if any text input field has changed
+$("input").on("input", function () {
+  $("#saveChangeBtn").show();
+});
+
+
+// EVENT LISTENERS (end) -----------------------------------
 
 
 
+// Get todays day and parse into id
+const todaysDay = moment().format("dddd");
+$("#day-today").text(todaysDay);
+
+
+// Array of times to check against
 const BlocksAsTime = ["9:00am","10:00am","11:00am","12:00pm","1:00pm","2:00pm","3:00pm","4:00pm","5:00pm"]
 
+
+// Array of all 9 input textarea to apply style to
 const allBlocksIds = ["#num9","#num10","#num11","#num12","#num1","#num2","#num3","#num4","#num5"]
 
-console.log(allBlocksIds)
 
-
+// Colour textarea if an hr has passed or it's the current hr
 function timeChecker() {
-  const currentDateTime = moment();
+  const currentDateTime = moment(); // gets current day/time
 
   for(let i = 0; i < BlocksAsTime.length; i++) {
-
-    let blockTime = moment(BlocksAsTime[i], "h:mm a")
+    let blockTime = moment(BlocksAsTime[i], "h:mm a") // gets a time as a moment object
 
     if (currentDateTime.isAfter(blockTime)) {
       $(allBlocksIds[i]).css('background-color', 'red')
@@ -28,18 +42,14 @@ function timeChecker() {
     if (currentDateTime.hour() === blockTime.hour()) {
       $(allBlocksIds[i]).css('background-color', 'green')
     } 
-    
-    
-
   }
-
-
 }
-setInterval(timeChecker, 1000);
+setInterval(timeChecker, 1000); // runs the timeChecker func every second
 
 
 
-// timeChecker()
+
+
 
 
 
